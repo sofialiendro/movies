7be0bf371797eccdc6ca48e78765d02afe5b6ece
Sofia Liendro <sofialiendro.s@gmail.com>
2021-06-14 20:21:27 -0300

    cambio favicon

--- src/utils/variables.js
+++ src/utils/variables.js
@@ -1,8 +1,5 @@
 export const APIKEY = "ddc431acb0e32208ae440feadb8c3681";
-export const URL_POPULAR_SERIES = 'https://api.themoviedb.org/3/tv/popular?'
 export const URL_POPULAR_MOVIES = 'https://api.themoviedb.org/3/movie/popular?'
-export const URL_LIVE_NOW_SERIES = 'https://api.themoviedb.org/3/tv/on_the_air?'
-export const URL_BEST_SERIES = 'https://api.themoviedb.org/3/tv/top_rated?'
 export const URL_BEST_MOVIES = 'https://api.themoviedb.org/3/movie/top_rated?'
 export const URL_NEW_MOVIES = 'https://api.themoviedb.org/3/movie/upcoming?'
 export const URL_CINEMA_MOVIES = 'https://api.themoviedb.org/3/movie/now_playing?'
